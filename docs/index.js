545917142fd4c47d7ba3c4dacdeb8fa9ad066345
TuuKeZu <moilanentuukka5@gmail.com>
2022-02-27 19:19:05 +0200

    Commit II - Chart with chart.js

--- docs/index.js
+++ docs/index.js
@@ -2,14 +2,39 @@ document.addEventListener('DOMContentLoaded', () => {
     Initialize();
 });
 
-const Initialize = () => {
+const menu = {
+    active: false
+}
 
+const Initialize = () => {
+    initializeButtons();
     getCurrent();
 
     setInterval(() => {
         getCurrent();
     }, 30000);
 
+    getChartData();
+}
+
+const initializeButtons = () => {
+    document.getElementById('RETURN').addEventListener('click', () => { onClick('RETURN'); })
+    document.getElementById('OPEN').addEventListener('click', () => { onClick('GRAPH-OPEN'); })
+}
+
+const onClick = (action = String) => {
+    const GRAPH_CONTAINER = document.getElementById('GRAPH-CONTAINER');
+
+    switch(action){
+        case 'RETURN':
+            GRAPH_CONTAINER.style.display = 'none';
+            menu.active = false;
+            break;
+        case 'GRAPH-OPEN':
+            GRAPH_CONTAINER.style.display = 'flex';
+            menu.active = true;
+            break;
+    }
 }
 
 const updateWeatherData = (data = {}) => {
@@ -30,6 +55,63 @@ const updateWeatherData = (data = {}) => {
     console.log('Successfully updated the current temperature.')
 }
 
+
+const drawGraph = async (data = []) => {
+
+    const WEEKDAYS = {
+        0: 'Sunday',
+        1: 'Monday',
+        2: 'Tuesday',
+        3: 'Wednesday',
+        4: 'Thursday',
+        5: 'Friday',
+        6: 'Saturday'
+    }
+
+    let xValues = [];
+    let yValues = [];
+    
+    data.forEach((dataPoint, index) => {
+        yValues.push(Number.parseFloat(dataPoint.temperature))
+        const date = new Date(Date.parse(dataPoint.date));
+        xValues.push(`${WEEKDAYS[date.getDay()]} ${formatAMPM(date)}`)
+    });
+
+
+    Chart.defaults.global.defaultFontColor = 'white'
+    
+    new Chart("CHART", {
+        type: "line",
+        data: {
+            labels: xValues,
+            datasets: [{
+                label: 'Temperature',
+                pointRadius: 3,
+                pointBackgroundColor: "aqua",
+                backgroundColor: "rgba(0, 0, 0, 0.1)",
+                borderColor: "rgba(0, 0, 0, 0.3)",
+                data: yValues
+            }]
+        },
+        options: {
+            resposive: true,
+            title: {
+                display: true,
+                text: 'Observed temperature for the last 48h',
+                color: 'white'
+            },
+            legend: {
+                display: false,
+            },
+            scales: {
+                xAxes: [],
+                yAxes: [{ticks: {min: -20, max:20}}],
+            }
+        }
+    });
+
+}
+
 const getCurrent = async () => {
     const url = 'http://45.32.234.225:3000/api/get/now';
 
@@ -45,17 +127,28 @@ const getCurrent = async () => {
     });
 }
 
-const getLastHour = async () => {
-    const url = 'http://45.32.234.225:3000/api/get/now';
+const getChartData = async () => {
+    const url = 'http://45.32.234.225:3000/api/get/hours/48';
 
     fetch(url)
     .then(response => {
         response.json()
         .then(body => {
-            updateWeatherData(body);
+            drawGraph(body);
         });
     })
     .catch(err => {
         console.error(err);
     });
+}
+
+function formatAMPM(date) {
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var ampm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0'+minutes : minutes;
+    var strTime = hours + ':' + minutes + ' ' + ampm;
+    return strTime;
 }
