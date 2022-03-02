f5b47c2aba2d4e2e07f0110e3f36be59dd6de4f8
TuuKeZu <moilanentuukka5@gmail.com>
2022-03-02 09:00:12 +0200

    Added https protocol functionality to the api

--- api/main.js
+++ api/main.js
@@ -2,13 +2,31 @@ const CONFIG = require('./config.json');
 
 const express = require('express');
 const router = require('./routers/router');
+const cors = require('cors');
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
 
 const app = express();
 
-app.use(express.json());
+// Certificate
+const privateKey = fs.readFileSync(CONFIG['private-key-path'], 'utf8');
+const certificate = fs.readFileSync(CONFIG['certificate-path'], 'utf8');
+const ca = fs.readFileSync(CONFIG['certificate-authority-path'], 'utf8');
 
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
+
+
+app.use(express.json());
+app.use(cors());
 app.use('/api/', router);
 
-app.listen(CONFIG.port, () => {
-console.log(`Listing on ${CONFIG.port}...`);
+const httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(CONFIG.port, () => {
+	console.log(`Listening on ${CONFIG.port}`);
 });
